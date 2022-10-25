34d427ecced4ea063272c2b657022b1ebdc95b70
Alejandro Sánchez Peralta <65984679+sanalexito@users.noreply.github.com>
2022-10-25 14:38:10 -0500

    Update VBA_&_R_pinta.R

--- VBA_&_R_pinta.R
+++ VBA_&_R_pinta.R
@@ -1,9 +1,8 @@
 library(RDCOMClient)
-source('D:/ENADIS/Calculadores/macro_pinta.R')
 #-------------------------------------------------------------------------------
-ruta_m <- "D:/Varios/correr_macro_R/correr_macro_R/macros_R/macro_pinta_asp.xlsm"
-ruta_est <- "D:/Varios/correr_macro_R/correr_macro_R/libros/III_experiencia_pagos_tramites_COPIA.xlsx"
-ruta_cvs <- "D:/Varios/correr_macro_R/correr_macro_R/libros/III_experiencia_pagos_tramites_solicitudes_encig2015_cv.xlsx"
+ruta_m <- "Ruta/ de la/macro_pinta_asp.xlsm"
+ruta_est <- "Ruta/libro para pintar/estimaciones.xlsx"
+ruta_cvs <- "Ruta/libro para pintar/coeficientes_variacion.xlsx"
 #setwd(dir)
 #-------------------------------------------------------------------------------
 macro_pinta(ruta_m = ruta_m, lib_est = ruta_est, lib_cv = ruta_cv )
